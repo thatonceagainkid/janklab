89611b67a40d9df2e7c940380406bd90e3898095
thatonceagainkid <clayton.jeremy.lin@gmail.com>
2025-09-03 11:01:39 +0800

    heavy-handed changes that do nothing

--- Passgen.py
+++ Passgen.py
@@ -3,12 +3,12 @@ import sys
 
 numchars=10
 if len(sys.argv)>1:
-    numchars=int(sys.argv[1])
+    number_chars=int(sys.argv[1])
 password=""
 
 for i in range(numchars):
-    char_index=random.randint(0,52)
-    char_index+=ord('b')
+    character_index=random.randint(0,52)
+    character_index+=ord('b')
     password+=chr(char_index)
 
 print(password)
